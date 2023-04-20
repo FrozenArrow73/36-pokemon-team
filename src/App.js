6adbafd60212a73f6a6010811cf51a84406d2018
Derek Wheeler <frozenarrow73@gmail.com>
2023-04-20 15:59:41 -0600

    can now add pokemon to team

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
 import Header from './components/Header';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import TeamScreen from './screens/TeamScreen';
 import DexScreen from './screens/DexScreen';
+import axios from 'axios';
 
 function App() {
 
   const [teamPage, setTeamPage] = useState(false)
+  const [pokemon, setPokemon] = useState([])
 
   const changePage = (value) => {
     if (teamPage === value) return
     setTeamPage(value)
   }
+
+  useEffect (()=> {
+    axios
+        .get(`https://pokeapi.co/api/v2/pokemon?limit=1000`)
+        .then((res)=> {
+            let newState = res.data.results.map((item, index) => {
+                item.img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png`
+                return item
+            })
+            setPokemon(newState)
+        })
+}, [])
+
   return (
     <div className="App">
       <Header />
@@ -19,7 +34,7 @@ function App() {
         <button onClick={() => changePage(true)}>Team</button>
         <button onClick={() => changePage(false)}>Dex</button>
       </div>
-      {teamPage ? <TeamScreen/> : <DexScreen/>}
+      {teamPage ? <TeamScreen/> : <DexScreen pokemon={pokemon}/>}
     </div>
   );
 }
